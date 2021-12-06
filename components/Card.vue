2dbca291d3373aa85e0c009fea9f1195acd4abf7
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2021-12-06 18:11:29 +0100

    feat: recovery of the articles in front thanks to the api news & create new component called card

---
+++ components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-gray-300 p-10">
+    <p>{{ article.publishedAt }}</p>
+    <p>{{ article.title }}</p>
+    <p>{{ article.description }}</p>
+    <p></p>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: [Object],
+      default: () => {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <h1>Maxime</h1>
+  <div>
+    <Card v-for="article in articles" :key="article.title" :article="article" />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios, error }) {
+    return await $axios
+      .$get(
+        'https://newsapi.org/v2/top-headlines?country=fr&pageSize=100&apiKey=005a93515fac4347beee2c4bf9365109'
+      )
+      .then((res) => {
+        // console.log(res.articles)
+        return { articles: res.articles }
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: 'The data has not been loaded' })
+      })
+  },
+}
 </script>
